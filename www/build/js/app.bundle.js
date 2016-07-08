c20876cc028258e45a91a7ce68b2ca413fba82c8
Matt Rothenberg <mattrothenberg@gmail.com>
2016-07-08 07:47:04 -0400

    reduce max zoom level

--- www/build/js/app.bundle.js
+++ www/build/js/app.bundle.js
@@ -913,7 +913,7 @@ var MapPage = (function () {
         }, 100);
         L.tileLayer('https://api.mapbox.com/styles/v1/mattrothenberg/ciq701j2s0019bymc8zits5e4/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWF0dHJvdGhlbmJlcmciLCJhIjoiY2lxNzAxM2k1MDBqN2ZxbTZwcXQ1cndicyJ9.JCea1zx6hAn6J8cWL0tGsg', {
             attribution: '',
-            maxZoom: 18,
+            maxZoom: 17,
             id: 'your.mapbox.project.id',
             accessToken: 'your.mapbox.public.access.token'
         }).addTo(mymap);
